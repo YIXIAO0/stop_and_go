50e96d5d60b8f66b5be1b7a7d3960b67d6a9052f
chihchunhsu <chihchunhsu@chihchunhsu.local>
2022-12-05 20:55:55 -0800

    Add four states in main loop

--- software/main.py
+++ software/main.py
@@ -1,11 +1,18 @@
 import os
 import time
 import cv2
+from enum import Enum
 from gps import GPS
 from obd import OBD
 from pushover import Pushover
 from detection import StopSignDetectionModel
 from matplotlib import pyplot as plt 
+
+class State(Enum):
+    DetectStopSign = 1
+    TrackVehicleSpeed = 2
+    Pass = 3
+    Fail = 4
 
 def main():
     # init
@@ -15,11 +22,28 @@ def main():
     # model = StopSignDetectionModel()
     # cap = cv2.VideoCapture(0)
 
+    state = State.DetectStopSign
 
     # main loop
     try:
         while True:
             time.sleep(0.1)
+            
+            if state == State.DetectStopSign:
+                print("detecting stop sign...")
+                state = State.TrackVehicleSpeed
+
+            elif state == State.TrackVehicleSpeed:
+                print("tracking vehicle speed...")
+                state = State.Pass
+
+            elif state == State.Pass:
+                print("passed!")
+                state = State.DetectStopSign
+
+            elif state == State.Fail:
+                print("failed!")
+                state = State.DetectStopSign
             
     except KeyboardInterrupt:
         pass
